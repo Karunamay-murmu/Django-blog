2020fefec2426152b65e98c8adf035951b8ea440
Karunamay Murmu <Karunamaymurmu.km@gmail.com>
2020-12-09 20:48:19 +0530

    add tab under profile icon

--- my_blog/static/js/blog.js
+++ my_blog/static/js/blog.js
@@ -1,25 +1,42 @@
 class Blog {
     constructor() { }
     static toggleSideMenu() {
-        try {
-            const burger = document.querySelector('#burger')
-            const cross = document.querySelector('#cross')
-            const nav = document.querySelector('#navigation-menu')
+        const burger = document.querySelector('#burger')
+        const cross = document.querySelector('#cross')
+        const nav = document.querySelector('#navigation-menu')
 
-            burger.onclick = function () {
-                nav.classList.add('showSideMenu')
-                cross.classList.add('showCross')
-            }
-
-            cross.onclick = function () {
-                nav.classList.remove('showSideMenu')
-                cross.classList.remove('showCross')
-            }
-
-        } catch (error) {
-            console.log(error)
+        burger.onclick = function () {
+            nav.classList.add('showSideMenu')
+            cross.classList.add('display')
         }
+
+        cross.onclick = function () {
+            nav.classList.remove('showSideMenu')
+            cross.classList.remove('display')
+        }
+    }
+
+    static async toggleProfileOptions() {
+        const profileIcon = document.getElementById('profileButton')
+        const optionCard = profileIcon.nextElementSibling;
+        let optionCardVisible = false
+
+        profileIcon.onclick = function (e) {
+            optionCard.classList.toggle('display');
+            optionCardVisible = true;
+
+            if (optionCardVisible) {
+                e.stopImmediatePropagation();
+                document.onclick = function () {
+                    optionCard.classList.remove('display')
+                }
+                optionCardVisible = false
+            }
+
+        }
+
     }
 }
 
 Blog.toggleSideMenu()
+Blog.toggleProfileOptions()
